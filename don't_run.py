4f1dfabd0c901ab568dc424ce5d73291c5fa7f24
mncrftfrcnm <64576881+mncrftfrcnm@users.noreply.github.com>
2023-09-22 17:24:13 +0400

    don't

---
+++ don't_run.py
@@ -0,0 +1,13 @@
+import pyautogui
+
+import time
+
+time.sleep(2)
+
+while True:
+
+    # to display the time at which the message is sent
+
+    pyautogui.typewrite("hahhahahhahahahahhahhahhahahhahahahahhah")
+    pyautogui.press("enter")
+    time.sleep(1)
